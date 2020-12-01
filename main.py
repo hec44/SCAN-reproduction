75255739ea179527b056cc377dbbde1bba89f04a
hec44 <hectormb.9@gmail.com>
2020-12-01 12:14:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from data import load_data
 from train import load_model,train
 
 
-def main():
+def main(path, name, in_ext, out_ext):
     train_iter,src_field,trg_field=load_data(path, name, in_ext, out_ext)
     model,optimizer,criterion=load_model(src_field,trg_field)
     train(model,train_iter,optimizer,criterion)
