8de91f40ba2bfe17795072d99d20ca2269c7ccc9
Conrad Donau Lauridsen <cdlauridsen@hotmail.com>
2020-12-07 19:33:27 +0100

    test.py

---
+++ test.py
@@ -0,0 +1,21 @@
+#test.py
+
+import os
+import torch
+from tqdm import tqdm
+import pdb
+
+cwd = os.getcwd()
+
+def test(test_iter, model):
+
+	model.eval()
+	with torch.no_grad():
+		for _, batch in tqdm(enumerate(test_iter)):
+
+			src = batch.src
+            trg = batch.trg
+
+            pdb.set_trace()
+
+            output = model(src, trg[0])
